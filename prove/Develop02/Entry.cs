10e44dc64ae13f3ef01d4e94ead36bad6f545825
QuinnAsher <pauldivineagionoja@yahoo.com>
2023-10-07 23:49:19 +0100

    added entry class

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,21 @@
+﻿namespace Develop02;
+
+// a class for generating an entry
+public class Entry
+{
+    public string _prompt;
+    public string _userInput;
+    public DateTime _dateStamp;
+
+    public Entry(string prompt, string userInput)
+    {
+        _prompt = prompt;
+        _userInput = userInput;
+        _dateStamp = DateTime.Now;
+    }
+
+    public string FormattedEntry()
+    {
+        return $"Dte: {_dateStamp} - {_prompt}\n{_userInput}";
+    }
+}
