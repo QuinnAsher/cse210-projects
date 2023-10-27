8c8fffa0906ef737c6069e64e054cc9e2e165726
quinnasher <pauldivineagionoja@yahoo.com>
2023-10-27 05:40:42 +0100

    fixed and updated this class, everything works well

--- prove/Develop03/SaveAndLoadScripture.cs
+++ prove/Develop03/SaveAndLoadScripture.cs
@@ -3,63 +3,170 @@
 public class SaveAndLoadScripture
 {
     private string _filePath;
-    private StreamWriter _writer;
-    private StreamReader _reader;
-    private bool _fileExit;
     private string _book;
-    private string _chapter;
+    private int _chapter;
     private int _startVerse;
     private int _endVerse;
     private string _scriptureText;
+    private bool _fileExit;
+    private Reference _reference;
+    private ScriptureLibrary _scriptureLibrary;
 
-
+    
    public SaveAndLoadScripture()
    {
-       _filePath = "scriptureData.csv";
+       _filePath = "scriptureData";
         _book = "John";
-        _chapter = "3";
+        _chapter = 3;
         _startVerse = 16;
         _endVerse = 0;
         _scriptureText =
             "For God so loved the world, that he gave his only begotten Son, " +
             "that whosoever believeth in him should not perish, but have everlasting life.";
-
-        _writer = new StreamWriter(_filePath);
-        _reader = new StreamReader(_filePath);
-        _fileExit = File.Exists(_filePath);
-    }
-   
-    public SaveAndLoadScripture(string filePath, string book, string chapter, int startVerse, int endVerse)
-    {
-        _book = book;
-        _chapter = chapter;
-        _startVerse = startVerse;
-        _endVerse = endVerse;
+        
         // check for valid extension
-        if (Path.HasExtension(filePath) && Path.GetExtension(filePath) != ".csv" || !Path.HasExtension(filePath))
+        if (Path.HasExtension(_filePath) && Path.GetExtension(_filePath) != ".csv" || !Path.HasExtension(_filePath))
         {
-            filePath = Path.ChangeExtension(filePath, ".csv");
+            _filePath = Path.ChangeExtension(_filePath, ".csv");
         }
+        
+        _fileExit = File.Exists(_filePath);  // checks whether a file exit
 
-        _filePath = filePath;
-        _writer = new StreamWriter(_filePath);
-    }
+   }
+   
+   
+   public SaveAndLoadScripture(string filePath, string book, int chapter, int startVerse, int endVerse)
+   {
+        
+       _book = book;
+       _chapter = chapter;
+       _startVerse = startVerse;
+       _endVerse = endVerse;
+        
+       // check for valid extension
+       if (Path.HasExtension(filePath) && Path.GetExtension(filePath) != ".csv" || !Path.HasExtension(filePath))
+       {
+           filePath = Path.ChangeExtension(filePath, ".csv");
+       }
+       _filePath = filePath;
+       
+       _fileExit = File.Exists(_filePath);  // checks whether a file exit
+   }
 
+   
+   // class getters and setters  // for reminder, if this does not return the word from the file move it down
+   public string BookProperty
+   {
+       get
+       {
+           return _book;
+       }
+
+       set
+       {
+           _book = value;
+       }
+   }
+
+   public int ChapterProperty
+   {
+       get
+       {
+           return _chapter;
+       }
+
+       set
+       {
+           _chapter = value;
+       }
+   }
+
+   public int StartVersePropery
+   {
+       get
+       {
+           return _startVerse;
+       }
+
+       set
+       {
+           _startVerse = value;
+       }
+   }
+
+   public int EndVerseProperty
+   {
+       get
+       {
+           return _endVerse;
+       }
+
+       set
+       {
+           _endVerse = value;
+       }
+   }
+
+   public string ScriptureTextProperty
+   {
+       get
+       {
+           return _scriptureText;
+       }
+
+       set
+       {
+           _scriptureText = value;
+       }
+   }
+   
+    
     public void WriteToFile()
     {
-        // if (_fileExit)  // checks if the file exits
-        // {
-        //     using (_writer)
-        //     {
-        //         _writer.WriteLine($"{_book},{_chapter},{_startVerse},{_endVerse},\"{_scriptureText}\"");
-        //     }
-        // }
-        
-        // else
-    
-        _writer.WriteLine($"{_book},{_chapter},{_startVerse},{_endVerse},\"{_scriptureText}\"");
-        _writer.WriteLine("Book,Chapter,Start Verse,End Verse,Scripture");
-      
-        
+        StreamWriter writer = new StreamWriter(_filePath, true);
+        if (_fileExit)  // file exits so just append to the file
+        {
+            // a colon is used as seperator for the csv because the scripture text haas space and commas
+            // so I have to use something that will not affect the code later
+            using (writer)
+            {
+                writer.WriteLine($"{_book};{_chapter};{_endVerse};{_endVerse};{_scriptureText}");
+            }
+        }
+
+        else
+        {
+            using (writer)
+            {
+                writer.WriteLine("Book,Chapter,Start Verse,End Verse,Scripture");
+                writer.WriteLine($"{_book};{_chapter};{_endVerse};{_endVerse};{_scriptureText}");
+            }
+        }
+    }
+
+    public void LoadFromFile()
+    {
+        string[] lines = File.ReadAllLines(_filePath);
+
+        foreach (string line in lines.Skip(1))  // skips the first line which is the header of the csv
+        {
+            string[] parts = line.Split(";");
+            _book= parts[0];
+            _chapter = int.Parse(parts[1]);
+            _startVerse = int.Parse(parts[2]);
+            _endVerse= int.Parse(parts[3]);
+            _scriptureText = parts[4];
+            
+            Reference reference = new Reference(_book, _chapter, _startVerse, _endVerse);
+            Scripture scripture = new Scripture(reference, _scriptureText);
+            ScriptureLibrary scriptureLibrary = new ScriptureLibrary();
+            
+            // Adds all the scriptures in from the ScriptureData csv to the scripture library list
+            scriptureLibrary.AddToLibraryList(reference, _scriptureText);
+
+
+        }
     }
 }
+
+// TODO: create a separate class for handing Loading because of too much fields
