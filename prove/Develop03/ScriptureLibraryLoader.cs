794824819603e1ced41ddacd51d6b24584e19c34
quinnasher <pauldivineagionoja@yahoo.com>
2023-10-28 13:47:12 +0100

    scriputure library loader class

---
+++ prove/Develop03/ScriptureLibraryLoader.cs
@@ -0,0 +1,80 @@
+﻿namespace Develop03;
+
+public class ScriptureLibraryLoader
+{
+    private string _filePath;
+    private string _book;
+    private bool _isExit;
+    private List<Dictionary<Reference, string>> _libraryList;
+    private Dictionary<Reference, string> _scriptureDict;
+    // private Reference _reference;
+
+    public ScriptureLibraryLoader(string filePath="scriptureData.csv")
+    {
+        _libraryList = new List<Dictionary<Reference, string>>();
+        _scriptureDict = new Dictionary<Reference, string>();
+        _filePath = filePath;
+        _isExit = File.Exists(_filePath);
+    }
+
+
+    public void AddToLibraryList(Reference reference, string scriptureText)
+    {
+        // create a dict with reference and text key and value respectively
+        _scriptureDict[reference] = scriptureText;
+        
+        // add the scriptureDict to the scripture list
+        _libraryList.Add(_scriptureDict);
+
+    }
+
+    public void LoadScripture()
+    {
+        try
+        {
+            if (_isExit)  // the fie exits because File.Exit returned true
+            {
+                string[] lines = File.ReadAllLines(_filePath);
+                
+                // iterate through the lines
+                foreach (string line in lines.Skip(1))  // skip the first line of the file
+                {
+                    string[] part = line.Split(";");
+                    
+                    // assign the values read from the file
+                    string book = part[0];
+                    int chapter = int.Parse(part[1]);
+                    int startVerse = int.Parse(part[2]);
+                    int endVerse = int.Parse(part[3]);
+                    string scriptureText = part[5];
+                    
+                    //NOTE: If the list is not updated, check the code below
+                    
+                    // instantiate and create a Reference object
+                    Reference reference = new Reference(book, chapter, startVerse, endVerse);
+                    
+                    // call the AddToLibraryList to add the library
+                    AddToLibraryList(reference, scriptureText);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+
+    public Tuple<Reference, string> GetReferenceAndScriptureText()
+    {
+        Random random = new Random();
+        int index = random.Next(_libraryList.Count);
+        
+        // select a random scripture from the list of scripture dictionary
+        Dictionary<Reference, string> selectedScripture = _libraryList[index];
+        KeyValuePair<Reference, string> keyValuePair = selectedScripture.Single();
+
+        return Tuple.Create(keyValuePair.Key, keyValuePair.Value);  // creates a tuple containing a scripture reference and text
+    }
+}
--- prove/Develop03/ScriputureLibrary.cs
+++ prove/Develop03/ScriputureLibrary.cs
@@ -1,55 +1,33 @@
-﻿namespace Develop03;
-
-
-public class ScriptureGenerator
-{
-    private List<Dictionary<Reference, string>> scriptureLibrary = new List<Dictionary<Reference, string>>();
-
-    public ScriptureGenerator()
-    {
-        // Create and initialize 20 default scriptures
-        for (int i = 0; i < 20; i++)
-        {
-            Dictionary<Reference, string> library = new Dictionary<Reference, string>
-            {
-                { new Reference("John", 3, 16), "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life." },
-                { new Reference("Psalm", 23, 1), "The Lord is my shepherd; I shall not want." },
-                { new Reference("Proverbs", 3, 5), "Trust in the Lord with all your heart, and lean not on your own understanding." },
-                { new Reference("Isaiah", 40, 31), "But they that wait upon the Lord shall renew their strength; they shall mount up with wings as eagles; they shall run, and not be weary; and they shall walk, and not faint." },
-                { new Reference("Matthew", 28, 19), "Go ye therefore, and teach all nations, baptizing them in the name of the Father, and of the Son, and of the Holy Ghost." },
-                { new Reference("1 Corinthians", 13, 4), "Love is patient, love is kind. It does not envy, it does not boast, it is not proud." },
-                { new Reference("Genesis", 1, 1), "In the beginning, God created the heavens and the earth." },
-                { new Reference("Romans", 8, 28), "And we know that all things work together for good to them that love God, to them who are the called according to his purpose." },
-                { new Reference("Ephesians", 2, 8), "For by grace are ye saved through faith; and that not of yourselves: it is the gift of God." },
-                { new Reference("Psalm", 19, 14), "Let the words of my mouth, and the meditation of my heart, be acceptable in thy sight, O Lord, my strength, and my redeemer." },
-                { new Reference("Proverbs", 4, 23), "Keep thy heart with all diligence; for out of it are the issues of life." },
-                { new Reference("Jeremiah", 29, 11), "For I know the thoughts that I think toward you, saith the Lord, thoughts of peace, and not of evil, to give you an expected end." },
-                { new Reference("John", 14, 6), "I am the way, the truth, and the life: no man cometh unto the Father, but by me." },
-                { new Reference("Romans", 12, 2), "And be not conformed to this world: but be ye transformed by the renewing of your mind, that ye may prove what is that good, and acceptable, and perfect, will of God." },
-                { new Reference("Philippians", 4, 13), "I can do all things through Christ which strengtheneth me." },
-                { new Reference("Psalm", 51, 10), "Create in me a clean heart, O God; and renew a right spirit within me." },
-                { new Reference("Colossians", 3, 23), "And whatsoever ye do, do it heartily, as to the Lord, and not unto men." },
-                { new Reference("1 Peter", 5, 7), "Casting all your care upon him; for he careth for you." },
-                { new Reference("2 Timothy", 1, 7), "For God hath not given us the spirit of fear; but of power, and of love, and of a sound mind." },
-                { new Reference("Revelation", 21, 4), "And God shall wipe away all tears from their eyes; and there shall be no more death, neither sorrow, nor crying, neither shall there be any more pain: for the former things are passed away." },
-
-            };
-
-            scriptureLibrary.Add(library);
-        }
-    }
-
-    public Tuple<Reference, string> GetRandomScripture()
-    {
-        Random random = new Random();
-        int libraryIndex = random.Next(scriptureLibrary.Count);
-        Dictionary<Reference, string> scriptureDict = scriptureLibrary[libraryIndex];
-
-        // Choose a random scripture from the selected library
-        int scriptureIndex = random.Next(scriptureDict.Count);
-        KeyValuePair<Reference, string> selectedScripture = scriptureDict.ElementAt(scriptureIndex);
-
-        // The selectedScripture is a KeyValuePair with a Reference as the key and the scripture text as the value
-        return new Tuple<Reference, string>(selectedScripture.Key, selectedScripture.Value);
-    }
-}
+﻿// namespace Develop03;
+//
+// public class ScriptureLibrary
+// {
+//     private List<Dictionary<Reference, string>> _libraryList;
+//     private Dictionary<Reference, string> scriptureDict;
+//
+//     public ScriptureLibrary()
+//     {
+//         _libraryList = new List<Dictionary<Reference, string>>();
+//         scriptureDict = new Dictionary<Reference, string>();
+//     }
+//
+//     public void AddToLibraryList(Reference reference, string scriptureText)
+//     {
+//         // scriptureDict.Add(reference, scriptureText);
+//         
+//         // A scripture dictionary with a key as reference and a value of scripture text
+//         scriptureDict[reference] = scriptureText;
+//         _libraryList.Add(scriptureDict);
+//     }
+//
+//     public Tuple<Reference, string> ReferenceAndScriptureText()
+//     {
+//         Random random = new Random();
+//         int index = random.Next(_libraryList.Count);
+//         // select a random scripture from the list of scriptures dictionary
+//         Dictionary<Reference, string> selectedScripture = _libraryList[index];
+//         KeyValuePair<Reference, string> keyValuePair = selectedScripture.Single();
+//
+//         return Tuple.Create(keyValuePair.Key, keyValuePair.Value);  // create a tuple containing a scripture reference and text
+//     }
+// }
