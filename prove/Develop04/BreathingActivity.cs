39177c45c72d0468d5f871223db8cb6df026d9a9
quinnasher <pauldivineagionoja@yahoo.com>
2023-11-05 05:16:01 +0100

    finished the breadthig activity

---
+++ prove/Develop04/BreathingActivity.cs
@@ -0,0 +1,42 @@
+﻿namespace Develop04;
+
+public class BreathingActivity : Activity
+{
+
+    public BreathingActivity(string name, string description, int duration) : base(name, description, duration)
+    {
+        ActivityNameProperty = name;
+        ActivityDescriptionProperty = description;
+        ActivityDurationProperty = duration;
+    }
+
+    public BreathingActivity()
+    {
+        ActivityNameProperty = "Breathing Activity";
+        ActivityDescriptionProperty =
+            "This activity will help you relax by walking you through breathing in and out slowly. clear your mind and focus on your breathing";
+        ActivityDurationProperty = 20;
+    }
+    
+  public void RunActivity()
+    {
+        DisplayStartMsg();
+        Console.WriteLine("Get ready....");
+        DisplaySpinner();
+        Console.WriteLine();
+        for (int i = 0; CalculateRunCycle() > i; i++)
+        {
+            Console.Write("Breathe in....");
+            DisplayCountDown();
+            Console.WriteLine();
+            
+            Console.Write("Breathe out....");
+            DisplayCountDown();
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+        
+        DisplayEndMsg();
+    }
+ 
+}
