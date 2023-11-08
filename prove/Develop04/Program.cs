b1230cda5042a9847fc62cd30ea7d7723f2ab66c
quinnasher <pauldivineagionoja@yahoo.com>
2023-11-08 05:17:55 +0100

    removed some reduntant codes in the Program class

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -5,19 +5,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Activity activity = new Activity("Breathing Activity", "This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and focus in you breathing", 10);
-        
-
-
-        // BreathingActivity breathing = new BreathingActivity();
-        // breathing.RunActivity();
-
-        ReflectiveActivity reflectiveActivity = new();
-        // reflectiveActivity.RunActivity();
-
         Menu menu = new Menu();
         menu.RunMenu();
-        ListingActivity listingActivity = new ListingActivity();
-        // listingActivity.RunActivity();
     }
 }
