d9596bc516f310fb13cc76e003e5bcc1c576fb7b
quinnasher <pauldivineagionoja@yahoo.com>
2023-11-19 18:06:58 +0100

    Implemented prevention of duplicate goal loading. This resolves the issue where loading goals from the same file would result in duplicates.

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -6,22 +6,5 @@ class Program
     {
         Menu menu = new Menu();
         menu.RunProgram();
-
-        Goal simpleGoal = new SimpleGoal("Sacrament Talk", "Give a sacrament talk when asked", 100);
-        Goal eternalGoal = new EternalGoal("Study Scriptures", "Study the scriptures daily", 100);
-        Goal checkListGoal = new CheckListGoal("Visit the Temple", "Visit the temple 3 times", 100, 500, 3);
-
-        GoalManager goalManager = new GoalManager();
-        //
-        // // goalManager.AddGoals(simpleGoal);
-        // // goalManager.AddGoals(eternalGoal);
-        // // goalManager.AddGoals(checkListGoal);
-        //
-        // goalManager.DisplayGoal();
-        // // goalManager.SaveGoalToFile("unique");
-        // // goalManager.LoadGoalFromFile("unique");
-        // goalManager.LoadGoalFromFile("unique");
-        goalManager.DisplayGoal();
-
     }
 }
