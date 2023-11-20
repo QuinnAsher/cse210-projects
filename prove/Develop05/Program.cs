79f97c073b99a2d544a463efdae3e4d82c556ef2
quinnasher <pauldivineagionoja@yahoo.com>
2023-11-20 10:38:42 +0100

    forgot to uncomment RunProgram method

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -5,7 +5,7 @@ class Program
     public static void Main(string[] args)
     {
         Menu menu = new Menu();
-        // menu.RunProgram();
+        menu.RunProgram();
 
         // Goal checkListGoal = new CheckListGoal("Visit the Temple", "I want to visit the temple 3 times this year", 1000,
         //     1000, 3);
